d9a66d7494d7842568d30c8acdbdbc46bc9afa90
Brandon Lehmann <brandonlehmann@gmail.com>
2019-07-28 10:21:02 -0400

    Introducing Chukwa: TurtleCoin's Argon2id PoW - Argon2 Implementation from: https://github.com/WOnder93/argon2   - Updated CPU feature detection - Chukwa - Argon2id Properties: 512KB, 1 parallelism, 3 iterations. - Hard Fork @ block 1,800,000 - Difficulty reset at block 1,800,000

--- src/cryptonotecore/Difficulty.h
+++ src/cryptonotecore/Difficulty.h
@@ -13,6 +13,8 @@ uint64_t nextDifficultyV4(std::vector<uint64_t> timestamps, std::vector<uint64_t
 
 uint64_t nextDifficultyV3(std::vector<uint64_t> timestamps, std::vector<uint64_t> cumulativeDifficulties);
 
+uint64_t adjustForDifficultyReset(const uint64_t nextDifficulty, const uint64_t blockIndex);
+
 template<typename T> T clamp(const T &n, const T &lower, const T &upper)
 {
     return std::max(lower, std::min(n, upper));
